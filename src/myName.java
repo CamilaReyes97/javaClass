902ba8fd75c692abbc43293cfcaf681d282a5e6f
CamilaReyes97 <camila.reyesber97@gmail.com>
2024-01-19 22:44:32 -0500

    Module 1 - Programming Exercise

---
+++ src/myName.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+public class myName {
+    public static void main(String[] args) {
+        System.out.println("Laura Camila Reyes");
+        JOptionPane.showMessageDialog(null, "Laura Camila Reyes");
+    }
+}
